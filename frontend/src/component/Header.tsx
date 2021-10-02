e5bfea080728c25b3cf883a9468e8d65e0770796
riroan <riroan@naver.com>
2021-10-03 01:47:16 +0900

    chore: add padding

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const Header = ()=> {
     return (
         <Back>
-            <img src="http://placehold.it/200x100" alt="logo" />
+            <img src="http://placehold.it/150x75" alt="logo" />
             <Form>
                 <Input type="text" />
                 <Button type="submit">
@@ -19,6 +19,7 @@ const Header = ()=> {
 export default Header;
 
 const Back = styled.div`
+    padding:5px;
     background-color:yellow;
     display:flex;
     align-items:center;
@@ -47,8 +48,8 @@ const Button = styled.button`
     box-sizing:border-box;
     font-size:1.5rem;
     font-weight:700;
-    width:2rem;
-    height:2rem;
+    width:2.3rem;
+    height:2.3rem;
     margin:0.2rem;
 
     &:hover{
