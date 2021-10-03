bb40cf315526945b37e4bff01781ea858c90b108
riroan <riroan@naver.com>
2021-10-03 17:24:24 +0900

    add: default structure

---
+++ frontend/src/component/Match.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Match=()=> {
+    return (
+        <Back>
+            MATCH
+        </Back>
+    )
+}
+
+export default Match;
+
+const Back = styled.div`
+    display:inline-block;
+    background-color:magenta;
+    width:70%;
+    padding : 20px;
+    box-sizing:border-box;
+`
---
+++ frontend/src/component/Most.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Most=()=> {
+    return (
+        <Back>
+            HI
+        </Back>
+    )
+}
+
+export default Most;
+
+const Back = styled.div`
+    display:inline-block;
+    width:30%;
+    background-color:orange;
+    padding:20px;
+    box-sizing:border-box;
+`
